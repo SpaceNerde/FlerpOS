95d1e746d791e24669f9d19d346667666099993f
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-02-21 14:57:41 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@
 // if it not works use this command
 // cargo build --target thumbv7em-none-eabihf
 
+// build
+// 1: cargo install bootimage
+// 2: cargo bootimage
+// 3: cargo build (start from here if u did steps above)
+// 4: cargo run
+
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-static HELLO: &[u8] = b"Hello, World!";
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -23,8 +24,14 @@ pub extern "C" fn _start() -> ! {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
+        }        
     }
-    
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
