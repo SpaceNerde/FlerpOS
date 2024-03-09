b86ef2d0a2fa42e19593207510385bdb57e4e698
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-03-09 17:19:08 +0100

    rewrite main into lib for intergration test

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(flerp_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
-
-use core::fmt::Write;
 use core::panic::PanicInfo;
+use flerp_os::println;
 
 // main logic
 #[no_mangle]
@@ -50,76 +47,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-
-    loop {}
-}
-
-//------------------------------------------------
-//
-// Test Logic
-//
-//------------------------------------------------
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-// simple test runner for debugging and other stuff
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-//------------------------------------------------
-//
-// Qemu Logic
-//
-//------------------------------------------------
-
-// make qemu competible with the test runner so it does not crash or anything like that!
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-//------------------------------------------------
-//
-// Tests
-//
-//------------------------------------------------
-
-// test to test the test runnner? lol
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    flerp_os::test_panic_handler(info)
 }
